623050012ff2374f79d62dff4b0b1a6c84612e45
Josh <wegman7@gmail.com>
2025-10-26 13:09:31 -0400

    cleanup

--- notebooks/adp_vs_ecr.py
+++ notebooks/adp_vs_ecr.py
@@ -1,3 +1,17 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.18.1
+#   kernelspec:
+#     display_name: .venv
+#     language: python
+#     name: python3
+# ---
+
 # %% [markdown]
 # ADP vs ECR analysis driver
 #
